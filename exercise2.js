cc3d5e244aae050def5913d27f3d7e3cb645254e
Budi Gunawan <budiindra.g@gmail.com>
2020-02-04 10:49:32 +0700

    merubah tipe data variabel hasil

--- exercise2.js
+++ exercise2.js
@@ -1,9 +1,9 @@
 var teks = 'hello world!';
 var teksArray = teks.split('');
-var hasil = [];
+var hasil = '';
 
 for (var i = teksArray.length-1; i >= 0; i--) {
-    hasil.push(teksArray[i]);
+    hasil += teksArray[i];
 }
 
-console.log(hasil.join(''));
+console.log(hasil);
