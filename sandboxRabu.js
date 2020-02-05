f3028a062c37cdd202cf9747001068fb0f3452e7
Budi Gunawan <budiindra.g@gmail.com>
2020-02-05 14:55:23 +0700

    membuat sandboxRabu

---
+++ sandboxRabu.js
@@ -0,0 +1,54 @@
+// var todos = [
+//     ['makan', 3],
+//     ['belajar', 2],
+//     ['game', 1]
+// ]
+
+// var log = ['makan','belajar','game'];
+
+// for (var i = 0; i < log.length; i++) {
+//     for (var j = 0; j < todos.length; j++) {
+//         if (log[i] === todos[j][0]) {
+//             todos[j][1]++;
+//         }
+//     }
+// }
+
+// console.log(todos);
+
+// function number(rows, columns) {
+//     var newArray = [];
+//     var x = 1;
+
+//     for (var i = 0; i < rows; i++){
+//         newArray[i] = [];
+
+//         for (var j = 0; j < columns; j++) {
+//             newArray[i].push(x++);
+//         }
+//     }
+
+//     return newArray;
+// }
+
+
+///[[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+
+// function kotak (baris, kolum){
+//     var buat = [];
+//     var x = 1;
+
+//     for ( var i = 0; i < baris; i++){
+//         buat[i] = [];
+
+//         for (var j = 0; j < kolum; j++){
+//             buat[i].push(x++)
+//         }
+//     } return buat
+// } 
+
+
+
+
+// console.log(kotak(4,3));
+
