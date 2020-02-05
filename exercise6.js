b6c8eb32d3f14be96a3e8b8f6d2eee3e517f40c5
Budi Gunawan <budiindra.g@gmail.com>
2020-02-05 13:51:14 +0700

    membuat exercise 6 palindrome angka

---
+++ exercise6.js
@@ -0,0 +1,19 @@
+function angkaPalindrome(num) {
+    if (num < 9) {
+        return num+1;
+    }
+    else {
+        var hasil = num + 1;
+        while(hasil){
+            var balikHasil = '';
+            
+        }
+    }
+  }
+  
+  // TEST CASES
+  console.log(angkaPalindrome(8)); // 9
+  console.log(angkaPalindrome(10)); // 11
+  console.log(angkaPalindrome(117)); // 121
+  console.log(angkaPalindrome(175)); // 181
+  console.log(angkaPalindrome(1000)); // 1001
