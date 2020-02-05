2acbe9e7eaa7dc62acf732c19c465ee3f6fe5847
Budi Gunawan <budiindra.g@gmail.com>
2020-02-05 21:46:32 +0700

    membuat exercise11 deret aritmatika

---
+++ exercise11.js
@@ -0,0 +1,22 @@
+function tentukanDeretAritmatika(arr) {
+    var beda = arr[1] - arr[0];
+    var hasil;
+
+    for (let i = 1; i <= arr.length-1; i++) {
+        if (arr[i]-arr[i-1] == beda) {
+            hasil = true;
+        }
+        else {
+            hasil = false;
+            break;
+        }
+    }
+  return hasil;
+}
+
+// TEST CASES
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
+console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
+console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
+console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
+console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
