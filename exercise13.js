27d22c68b4fa95849d1d8777ef27c39fe1491437
Budi Gunawan <budiindra.g@gmail.com>
2020-02-05 22:08:00 +0700

    membuat exercise13 target terdekat

---
+++ exercise13.js
@@ -0,0 +1,34 @@
+function targetTerdekat(arr) {
+    var posX = [];
+    var posO = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] === 'x') {
+            posX.push(i);
+        }
+        else if (arr[i] === 'o') {
+            posO.push(i);
+        }
+    }
+
+    if (posX.length == 0) {
+        return 0;
+    }
+    else {
+        var hasil = 0;
+        for (let j = 0; j < posX.length; j++){
+            var temp = Math.abs(posX[j]-posO[0]);
+            if (hasil == 0 || temp < hasil){
+                hasil = temp;
+            }
+        }
+    }
+    return hasil;
+}
+
+// TEST CASES
+console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
+console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
+console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
+console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
+console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
